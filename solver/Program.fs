07030c71c0f74b5dd65ec58def81eaf70fb05d09
AshleyF <ashley.feniello@live.com>
2018-05-22 19:59:53 -0700

    Solves DL CL

--- solver/Program.fs
+++ solver/Program.fs
@@ -1,5 +1,7 @@
 ﻿open System
 
+let interactive = false // affects pause
+
 type Color = R | O | W | Y | B | G
 type Face = U | D | L | R | F | B
 type Sticker = UL | U | UR | L | C | R | DL | D | DR
@@ -23,16 +25,16 @@ let solved =
     cubeOfFaces u d l r f b
 
 let renderWithHighlights highlights cube =
-    let renderFace f x y =
+    let renderFace f x y slice =
         let renderSticker s =
             let setColorForWindows () =
                 let toConsoleColor hilited = function
-                    | Color.R -> if hilited then ConsoleColor.Red else ConsoleColor.DarkRed
-                    | Color.O -> if hilited then ConsoleColor.Magenta else ConsoleColor.DarkMagenta
-                    | Color.W -> if hilited then ConsoleColor.White else ConsoleColor.Gray
-                    | Color.Y -> if hilited then ConsoleColor.Yellow else ConsoleColor.DarkYellow
-                    | Color.B -> if hilited then ConsoleColor.Blue else ConsoleColor.DarkBlue
-                    | Color.G -> if hilited then ConsoleColor.Green else ConsoleColor.DarkGreen
+                    | Color.R -> if hilited then ConsoleColor.Red else ConsoleColor.DarkGray // ConsoleColor.DarkRed
+                    | Color.O -> if hilited then ConsoleColor.Magenta else ConsoleColor.DarkGray // ConsoleColor.DarkMagenta
+                    | Color.W -> if hilited then ConsoleColor.White else ConsoleColor.DarkGray // ConsoleColor.Gray
+                    | Color.Y -> if hilited then ConsoleColor.Yellow else ConsoleColor.DarkGray // ConsoleColor.DarkYellow
+                    | Color.B -> if hilited then ConsoleColor.Blue else ConsoleColor.DarkGray // ConsoleColor.DarkBlue
+                    | Color.G -> if hilited then ConsoleColor.Green else ConsoleColor.DarkGray // ConsoleColor.DarkGreen
                 let hilited = List.length highlights = 0 || List.contains (f, s) highlights
                 Console.BackgroundColor <- cube |> Map.find f |> Map.find s |> toConsoleColor hilited
             let setColorForLinuxAndMac () =
@@ -45,30 +47,50 @@ let renderWithHighlights highlights cube =
                     | Color.G -> if hilited then 10  else 22
                 let hilited = List.length highlights = 0 || List.contains (f, s) highlights
                 printf "\u001b[48;5;%im" (cube |> Map.find f |> Map.find s |> toConsoleColor hilited)
-            setColorForLinuxAndMac ()
+            // setColorForLinuxAndMac ()
+            setColorForWindows ()
             printf "  "
-        Console.CursorLeft <- x
-        Console.CursorTop <- y
-        renderSticker Sticker.UL
-        renderSticker Sticker.U
-        renderSticker Sticker.UR
-        Console.CursorLeft <- x
-        Console.CursorTop <- y + 1
-        renderSticker Sticker.L
-        renderSticker Sticker.C
-        renderSticker Sticker.R
-        Console.CursorLeft <- x
-        Console.CursorTop <- y + 2
-        renderSticker Sticker.DL
-        renderSticker Sticker.D
-        renderSticker Sticker.DR
-    Console.Clear()
-    renderFace Face.B 8 1
-    renderFace Face.U 8 4
-    renderFace Face.L 2 7
-    renderFace Face.F 8 7
-    renderFace Face.R 14 7
-    renderFace Face.D 8 10
+        if interactive then 
+            Console.CursorLeft <- x
+            Console.CursorTop <- y
+        if interactive || slice = 0 then renderSticker Sticker.UL
+        if interactive || slice = 0 then renderSticker Sticker.U
+        if interactive || slice = 0 then renderSticker Sticker.UR
+        if interactive then 
+            Console.CursorLeft <- x
+            Console.CursorTop <- y + 1
+        if interactive || slice = 1 then renderSticker Sticker.L
+        if interactive || slice = 1 then renderSticker Sticker.C
+        if interactive || slice = 1 then renderSticker Sticker.R
+        if interactive then 
+            Console.CursorLeft <- x
+            Console.CursorTop <- y + 2
+        if interactive || slice = 2 then renderSticker Sticker.DL
+        if interactive || slice = 2 then renderSticker Sticker.D
+        if interactive || slice = 2 then renderSticker Sticker.DR
+    if interactive then Console.Clear()
+    if interactive then 
+        renderFace Face.B 8 1 0
+        renderFace Face.U 8 4 0
+        renderFace Face.L 2 7 0
+        renderFace Face.F 8 7 0
+        renderFace Face.R 14 7 0
+        renderFace Face.D 8 10 0
+    else
+        let fill s = Console.BackgroundColor <- ConsoleColor.Black; printf s
+        let newline () = Console.BackgroundColor <- ConsoleColor.Black; printfn ""
+        fill "        "; renderFace Face.B 0 0 0; newline ()
+        fill "        "; renderFace Face.B 0 0 1; newline ()
+        fill "        "; renderFace Face.B 0 0 2; newline ()
+        fill "        "; renderFace Face.U 0 0 0; newline ()
+        fill "        "; renderFace Face.U 0 0 1; newline ()
+        fill "        "; renderFace Face.U 0 0 2; newline ()
+        fill "  "; renderFace Face.L 0 0 0; renderFace Face.F 0 0 0; renderFace Face.R 0 0 0; newline ()
+        fill "  "; renderFace Face.L 0 0 1; renderFace Face.F 0 0 1; renderFace Face.R 0 0 1; newline ()
+        fill "  "; renderFace Face.L 0 0 2; renderFace Face.F 0 0 2; renderFace Face.R 0 0 2; newline ()
+        fill "        "; renderFace Face.D 0 0 0; newline ()
+        fill "        "; renderFace Face.D 0 0 1; newline ()
+        fill "        "; renderFace Face.D 0 0 2; newline ()
     Console.BackgroundColor <- ConsoleColor.Black
     printfn ""
 
@@ -122,7 +144,6 @@ let rotateX' cube =
     cubeOfFaces u d l r f b
 
 let rotateX2 = rotateX >> rotateX
-let rotateX2' = rotateX2
 
 let rotateY cube =
     let u = faceU cube |> faceRotateCW
@@ -143,7 +164,6 @@ let rotateY' cube =
     cubeOfFaces u d l r f b
 
 let rotateY2 = rotateY >> rotateY
-let rotateY2' = rotateY2
 
 let rotateZ cube =
     let u = faceL cube |> faceRotateCW
@@ -164,7 +184,6 @@ let rotateZ' cube =
     cubeOfFaces u d l r f b
 
 let rotateZ2 = rotateZ >> rotateZ
-let rotateZ2' = rotateZ2
 
 let moveU cube =
     let l = faceL cube
@@ -209,81 +228,78 @@ let moveU' cube =
     cubeOfFaces u' d' l' r' f' b'
 
 let moveU2 = moveU >> moveU
-let moveU2' = moveU2
 
-let moveD = rotateX2 >> moveU >> rotateX2'
-let moveD' = rotateX2 >> moveU' >> rotateX2'
+let moveD = rotateX2 >> moveU >> rotateX2
+let moveD' = rotateX2 >> moveU' >> rotateX2
 let moveD2 = moveD >> moveD
-let moveD2' = moveD2
 
 let moveL = rotateZ >> moveU >> rotateZ'
 let moveL' = rotateZ >> moveU' >> rotateZ'
 let moveL2 = moveL >> moveL
-let moveL2' = moveL2
 
 let moveR = rotateZ' >> moveU >> rotateZ
 let moveR' = rotateZ' >> moveU' >> rotateZ
 let moveR2 = moveR >> moveR
-let moveR2' = moveR2
 
 let moveF = rotateX >> moveU >> rotateX'
 let moveF' = rotateX >> moveU' >> rotateX'
 let moveF2 = moveF >> moveF
-let moveF2' = moveF2
 
 let moveB = rotateX' >> moveU >> rotateX
 let moveB' = rotateX' >> moveU' >> rotateX
 let moveB2 = moveB >> moveB
-let moveB2' = moveB2
 
 let moveM = rotateX' >> moveL' >> moveR
 let moveM' = rotateX >> moveL >> moveR'
 let moveM2 = moveM >> moveM
-let moveM2' = moveM2
 
 let moveE = rotateY' >> moveU >> moveD'
 let moveE' = rotateY >> moveU' >> moveD
 let moveE2 = moveE >> moveE
-let moveE2' = moveE2
 
 let moveS = rotateZ >> moveF' >> moveB
 let moveS' = rotateZ' >> moveF >> moveB'
 let moveS2 = moveS >> moveS
-let moveS2' = moveS2
 
 let moveUW = rotateY >> moveD
 let moveUW' = rotateY' >> moveD'
+let moveUW2 = moveUW >> moveUW
 
 let moveDW = rotateY' >> moveU
 let moveDW' = rotateY >> moveU'
+let moveDW2 = moveDW >> moveDW
 
 let moveLW = rotateX' >> moveR
 let moveLW' = rotateX >> moveR'
+let moveLW2 = moveLW >> moveLW
 
 let moveRW = rotateX >> moveL
 let moveRW' = rotateX' >> moveL'
+let moveRW2 = moveRW >> moveRW
 
 let moveFW = rotateZ >> moveB
 let moveFW' = rotateZ' >> moveB'
+let moveFW2 = moveFW >> moveFW
 
 let moveBW = rotateZ' >> moveF
 let moveBW' = rotateZ >> moveF'
+let moveBW2 = moveBW >> moveBW
 
 type Rotate =
-    | X | X' | X2 | X2'
-    | Y | Y' | Y2 | Y2'
-    | Z | Z' | Z2 | Z2'
+    | X | X' | X2
+    | Y | Y' | Y2
+    | Z | Z' | Z2
 
 type Move =
-    | U | U' | U2 | U2' | UW | UW'
-    | D | D' | D2 | D2' | DW | DW'
-    | L | L' | L2 | L2' | LW | LW'
-    | R | R' | R2 | R2' | RW | RW'
-    | F | F' | F2 | F2' | FW | FW'
-    | B | B' | B2 | B2' | BW | BW'
-    | M | M' | M2 | M2'
-    | S | S' | S2 | S2'
-    | E | E' | E2 | E2'
+    | U | U' | U2 | UW | UW' | UW2
+    | D | D' | D2 | DW | DW' | DW2
+    | L | L' | L2 | LW | LW' | LW2
+    | R | R' | R2 | RW | RW' | RW2
+    | F | F' | F2 | FW | FW' | FW2
+    | B | B' | B2 | BW | BW' | BW2
+    | M | M' | M2
+    | S | S' | S2
+    | E | E' | E2
 
 type Step = Rotate of Rotate | Move of Move
 
@@ -291,65 +307,59 @@ let rotate = function
     | X   -> rotateX
     | X'  -> rotateX'
     | X2  -> rotateX2
-    | X2' -> rotateX2'
     | Y   -> rotateY
     | Y'  -> rotateY'
     | Y2  -> rotateY2
-    | Y2' -> rotateY2'
     | Z   -> rotateZ
     | Z'  -> rotateZ'
     | Z2  -> rotateZ2
-    | Z2' -> rotateZ2'
 
 let move = function
     | U   -> moveU
     | U'  -> moveU'
     | U2  -> moveU2
-    | U2' -> moveU2'
     | UW  -> moveUW
     | UW' -> moveUW'
+    | UW2 -> moveUW2
     | D   -> moveD
     | D'  -> moveD'
     | D2  -> moveD2
-    | D2' -> moveD2'
     | DW  -> moveDW
     | DW' -> moveDW'
+    | DW2 -> moveDW2
     | L   -> moveL
     | L'  -> moveL'
     | L2  -> moveL2
-    | L2' -> moveL2'
     | LW  -> moveLW
     | LW' -> moveLW'
+    | LW2 -> moveLW2
     | R   -> moveR
     | R'  -> moveR'
     | R2  -> moveR2
-    | R2' -> moveR2'
     | RW  -> moveRW
     | RW' -> moveRW'
+    | RW2 -> moveRW2
     | F   -> moveF
     | F'  -> moveF'
     | F2  -> moveF2
-    | F2' -> moveF2'
     | FW  -> moveFW
     | FW' -> moveFW'
+    | FW2 -> moveFW2
     | B   -> moveB
     | B'  -> moveB'
     | B2  -> moveB2
-    | B2' -> moveB2'
     | BW  -> moveBW
     | BW' -> moveBW'
+    | BW2 -> moveBW2
     | M   -> moveM
     | M'  -> moveM'
     | M2  -> moveM2
-    | M2' -> moveM2'
     | S   -> moveS
     | S'  -> moveS'
     | S2  -> moveS2
-    | S2' -> moveS2'
     | E   -> moveE
     | E'  -> moveE'
     | E2  -> moveE2
-    | E2' -> moveE2'
 
 let step = function
     | Rotate r -> rotate r
@@ -367,66 +377,60 @@ let rotationToString = function
     | X   -> "x"
     | X'  -> "x'"
     | X2  -> "x2"
-    | X2' -> "x2'"
     | Y   -> "y"
     | Y'  -> "y'"
     | Y2  -> "y2"
-    | Y2' -> "y2'"
     | Z   -> "z"
     | Z'  -> "z'"
     | Z2  -> "z2"
-    | Z2' -> "z2"
 let rotationsToString rots = String.Join(' ', Seq.map rotationToString rots)
 
 let moveToString = function
     | U   -> "U"
     | U'  -> "U'"
     | U2  -> "U2"
-    | U2' -> "U2'"
     | UW  -> "u"
     | UW' -> "u'"
+    | UW2 -> "u2"
     | D   -> "D"
     | D'  -> "D'"
     | D2  -> "D2"
-    | D2' -> "D2'"
     | DW  -> "d"
     | DW' -> "d'"
+    | DW2 -> "d2"
     | L   -> "L"
     | L'  -> "L'"
     | L2  -> "L2"
-    | L2' -> "L2'"
     | LW  -> "l"
     | LW' -> "l'"
+    | LW2 -> "l2"
     | R   -> "R"
     | R'  -> "R'"
     | R2  -> "R2"
-    | R2' -> "R2'"
     | RW  -> "r"
     | RW' -> "r'"
+    | RW2 -> "r2"
     | F   -> "F"
     | F'  -> "F'"
     | F2  -> "F2"
-    | F2' -> "F2'"
     | FW  -> "f"
     | FW' -> "f'"
+    | FW2 -> "f2"
     | B   -> "B"
     | B'  -> "B'"
     | B2  -> "B2"
-    | B2' -> "B2'"
     | BW  -> "b"
-    | BW' -> "b"
+    | BW' -> "b'"
+    | BW2 -> "bw"
     | M   -> "M"
     | M'  -> "M'"
     | M2  -> "M2"
-    | M2' -> "M2'"
     | S   -> "S"
     | S'  -> "S'"
     | S2  -> "S2"
-    | S2' -> "S2'"
     | E   -> "E"
     | E'  -> "E'"
     | E2  -> "E2"
-    | E2' -> "E2'"
 let movesToString moves = String.Join(' ', Seq.map moveToString moves)
 
 let stepToString = function
@@ -507,8 +511,6 @@ let searchCenters centers color cube =
     Seq.find find centers
 let findCenter = searchCenters [Center.U; Center.D; Center.L; Center.R; Center.F; Center.B]
 
-type EdgeOrientation = Oriented | Flipped
-
 let searchEdges edges color0 color1 cube =
     let rec find = function
         | e :: t ->
@@ -516,8 +518,7 @@ let searchEdges edges color0 color1 cube =
             | [f0, s0; f1, s1] ->
                 let l0 = look f0 s0 cube
                 let l1 = look f1 s1 cube
-                if l0 = color0 && l1 = color1 then e, Oriented
-                elif l1 = color0 && l0 = color1 then e, Flipped
+                if l0 = color0 && l1 = color1 || l1 = color0 && l0 = color1 then e, (l0, l1)
                 else find t
             | _ -> failwith "Expected exactly two edge stickers"
         | _ -> failwith "Edge not found"
@@ -525,8 +526,9 @@ let searchEdges edges color0 color1 cube =
 let findEdge = searchEdges [UL; UR; UF; UB; DL; DR; DF; DB; FL; FR; BL; BR]
 
 let pause () =
-    printfn "Press Enter to continue..."
-    Console.ReadLine() |> ignore
+    if interactive then 
+        printfn "Press Enter to continue..."
+        Console.ReadLine() |> ignore
 
 let execute steps message cube =
     let cube' = executeSteps steps cube
@@ -536,7 +538,41 @@ let execute steps message cube =
     cube'
 
 let roux cube =
-    let rotateCenterToLeftSide color cube =
+    let rotateDLEdgeIntoPosition colorD colorL cube =
+        let e, (c0, c1) = findEdge colorD colorL cube
+        renderWithHighlights (edgeToFaceStickers e) cube
+        printfn "Found %s/%s edge (%s/%s)" (colorToString colorL) (colorToString colorD) (colorToString c0) (colorToString c1)
+        pause ()
+        let cube' =
+            let task = "Rotate it to DL position"
+            match e, c0, c1 with
+            | UL, u, _ when u = colorD -> execute [Rotate X2] task cube
+            | UL, u, _ when u = colorL -> execute [Rotate Z'] task cube
+            | UR, u, _ when u = colorD -> execute [Rotate Z2] task cube
+            | UR, u, _ when u = colorL -> execute [Rotate X2; Rotate Z] task cube
+            | UF, u, _ when u = colorD -> execute [Rotate Z2; Rotate Y] task cube
+            | UF, u, _ when u = colorL -> execute [Rotate X'; Rotate Y] task cube
+            | UB, u, _ when u = colorD -> execute [Rotate X2; Rotate Y] task cube
+            | UB, u, _ when u = colorL -> execute [Rotate X; Rotate Y'] task cube
+            | DL, d, _ when d = colorD -> execute [] "Already in place!" cube
+            | DL, d, _ when d = colorL -> execute [Rotate X2; Rotate Z'] task cube
+            | DR, d, _ when d = colorD -> execute [Rotate Y2] task cube
+            | DR, d, _ when d = colorL -> execute [Rotate Z] task cube
+            | DF, d, _ when d = colorD -> execute [Rotate Y] task cube
+            | DF, d, _ when d = colorL -> execute [Rotate X2; Rotate Y'; Rotate Z'] task cube
+            | DB, d, _ when d = colorD -> execute [Rotate Y'] task cube
+            | DB, d, _ when d = colorL -> execute [Rotate X; Rotate Y] task cube
+            | FL, f, _ when f = colorD -> execute [Rotate X'] task cube
+            | FL, f, _ when f = colorL -> execute [Rotate Z'; Rotate Y] task cube
+            | FR, f, _ when f = colorD -> execute [Rotate X'; Rotate Y2] task cube
+            | FR, f, _ when f = colorL -> execute [Rotate Z; Rotate Y] task cube
+            | BL, b, _ when b = colorD -> execute [Rotate X] task cube
+            | BL, b, _ when b = colorL -> execute [Rotate X'; Rotate Z'] task cube
+            | BR, b, _ when b = colorD -> execute [Rotate X; Rotate Y2] task cube
+            | BR, b, _ when b = colorL -> execute [Rotate X; Rotate Z] task cube
+            | _ -> failwith "Unexpected edge position"
+        cube'
+    let moveCenterToLeftSide color cube =
         let c = findCenter color cube
         renderWithHighlights [centerToFaceSticker c] cube
         printfn "Found %s center" (colorToString color)
@@ -544,53 +580,48 @@ let roux cube =
         let cube' =
             let task = "Rotate it to left side"
             match c with
-            | Center.U -> execute [Rotate Z'] task cube
-            | Center.D -> execute [Rotate Z] task cube
-            | Center.L -> execute [] "Already in place" cube
-            | Center.R -> execute [Rotate Z2] task cube // or Z2', Y2, Y2'
-            | Center.F -> execute [Rotate Y] task cube
-            | Center.B -> execute [Rotate Y'] task cube
+            | Center.U -> execute [Move RW'; Move UW] task cube
+            | Center.D -> execute [Move RW; Move UW] task cube
+            | Center.L -> execute [] "Already in place!" cube
+            | Center.R -> execute [Move UW2] task cube // or Y2
+            | Center.F -> execute [Move UW] task cube
+            | Center.B -> execute [Move UW'] task cube
         cube'
-    let placeDLEdge colorD colorL cube =
-        let e, o = findEdge colorD colorL cube
-        renderWithHighlights (edgeToFaceStickers e) cube
-        printfn "Found %s/%s edge (%A)" (colorToString colorL) (colorToString colorD) o
-        pause ()
-        cube
-    let cube = rotateCenterToLeftSide Color.B cube // TODO: color neutral
-    let cube = placeDLEdge Color.W Color.B cube // TODO: color neutral
+    let down, left = Color.W, Color.B // TODO: color neutral
+    let cube = rotateDLEdgeIntoPosition down left cube
+    let cube = moveCenterToLeftSide Color.B cube // TODO: color neutral
     cube
 
 Console.BackgroundColor <- ConsoleColor.Black
 Console.ForegroundColor <- ConsoleColor.Gray
 Console.Clear()
 
-//(* highlight pieces
-for c in [Center.U; D; L; R; F; B] do
-    let stickers = centerToFaceSticker c
-    renderWithHighlights [stickers] solved
-    printfn "Center: %A" c
-    pause ()
-
-for e in [UL; UR; UF; UB; DL; DR; DF; DB; FL; FR; BL; BR] do
-    let stickers = edgeToFaceStickers e
-    renderWithHighlights stickers solved
-    printfn "Edge: %A" e
-    pause ()
-
-for c in [ULF; ULB; URF; URB; DLF; DLB; DRF; DRB] do
-    let stickers = cornerToFaceStickers c
-    renderWithHighlights stickers solved
-    printfn "Corner: %A" c
-    pause ()
-// *)
+let hiligePieces () =
+    for c in [Center.U; D; L; R; F; B] do
+        let stickers = centerToFaceSticker c
+        renderWithHighlights [stickers] solved
+        printfn "Center: %A" c
+        pause ()
+    for e in [UL; UR; UF; UB; DL; DR; DF; DB; FL; FR; BL; BR] do
+        let stickers = edgeToFaceStickers e
+        renderWithHighlights stickers solved
+        printfn "Edge: %A" e
+        pause ()
+    for c in [ULF; ULB; URF; URB; DLF; DLB; DRF; DRB] do
+        let stickers = cornerToFaceStickers c
+        renderWithHighlights stickers solved
+        printfn "Corner: %A" c
+        pause ()
 
 let testRoux () =
     let c, s = scramble 20
     render c
     printfn "Scramble: %s                  " (movesToString s)
     pause ()
-    roux c |> ignore
+    let c = roux c
+    if look Face.L Sticker.C c <> Color.B then failwith "Not solved LC"
+    if look Face.L Sticker.D c <> Color.B then failwith "Not solved DL"
+    if look Face.D Sticker.L c <> Color.W then failwith "Not solved DL"
     ignore
 
 while true do testRoux () |> ignore
