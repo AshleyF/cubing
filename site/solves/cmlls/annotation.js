8cd0e064d681a3831e783f8ac3edc97215ebdd6a
AshleyF <ashley.feniello@live.com>
2018-09-17 15:43:45 -0700

    CMLLs video

---
+++ site/solves/cmlls/annotation.js
@@ -0,0 +1,21 @@
+var annotation = {
+    frames: {
+        base: "cmlls/The%20Best%20CMLL%20Algorithms%20",
+        fps: 30,
+        first: 1,
+        last: 18810,
+    },
+    person: {
+        name: "Kian Mansour"
+    },
+    event: {
+        name: "The Best CMLL Algs"
+    },
+    scramble: "Unknown",
+    time: 0,
+    marks: [
+        { name: "O Adjacent Swap", frame: 163, type: "twist" },
+        { name: "...", frame: 392, type: "idle" },
+        { name: "O Diagonal Swap", frame: 511, type: "twist" },
+    ]
+}
