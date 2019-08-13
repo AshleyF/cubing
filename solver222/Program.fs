247d18e032fc180c4b950b483393566a4e31775a
AshleyF <ashley.feniello@live.com>
2019-08-12 18:38:27 -0700

    State sorting and faster load

--- solver222/Program.fs
+++ solver222/Program.fs
@@ -1,50 +1,22 @@
-﻿type State = int array * int array
+﻿open System
+open Utility
 
-let solvedState : State = [|0; 1; 2; 3; 4; 5; 6; 7|], [|0; 0; 0; 0; 0; 0; 0; 0|]
+let numStates = 3674160 
 
-type Transform = int array * int array
-
-type Twist = U | D | L | R | F | B
-
-let twistToTransform = function
-    | U -> [|1; 2; 3; 0; 4; 5; 6; 7|], [|0; 0; 0; 0; 0; 0; 0; 0|] : Transform
-    | D -> [|0; 1; 2; 3; 5; 6; 7; 4|], [|0; 0; 0; 0; 0; 0; 0; 0|] : Transform
-    | L -> [|0; 1; 6; 2; 4; 3; 5; 7|], [|0; 0; 2; 1; 0; 2; 1; 0|] : Transform
-    | R -> [|4; 0; 2; 3; 7; 5; 6; 1|], [|2; 1; 0; 0; 1; 0; 0; 2|] : Transform
-    | F -> [|3; 1; 2; 5; 0; 4; 6; 7|], [|1; 0; 0; 2; 2; 1; 0; 0|] : Transform
-    | B -> [|0; 7; 1; 3; 4; 5; 2; 6|], [|0; 2; 1; 0; 0; 0; 2; 1|] : Transform
-
-type Rotation = X | Y | Z
-
-let rotationToTransform = function
-    | Z -> [|3; 2; 6; 5; 0; 4; 7; 1|], [|1; 2; 1; 2; 2; 1; 2; 1|] : Transform
-    | X -> [|4; 0; 3; 5; 7; 6; 2; 1|], [|2; 1; 2; 1; 1; 2; 1; 2|] : Transform
-    | Y -> [|1; 2; 3; 0; 7; 4; 5; 6|], [|0; 0; 0; 0; 0; 0; 0; 0|] : Transform
-
-type Move = Twist of Twist | Rotation of Rotation
-
-let moveToTransform = function
-    | Twist t -> twistToTransform t
-    | Rotation r -> rotationToTransform r
-
-let applyTransform ((ps, os) : State) ((pt, ot) : Transform) : State =
-    Array.init 8 (fun i -> ps.[pt.[i]]),
-    Array.init 8 (fun i -> (os.[pt.[i]] + ot.[i]) % 3)
-
-let gen () =
-    let u = Twist U |> moveToTransform
-    let r = Twist R |> moveToTransform
-    let f = Twist F |> moveToTransform
+let getRawStates () =
+    // this generates all 3,674,160 possible 2x2 states, assuming DLB corner is fixed
+    printfn "%A Generating all states...
+             This one-time process may require ~15 minutes and 2GB RAM
+             States grows to %i, Queue grows to 1380349 and shrinks to zero" (DateTime.Now) numStates
     let nextPly progress states queue current =
-        if progress % 1000 = 0 then
-            printfn "States: %i Queue: %i" (Set.count states) (List.length queue)
+        if progress % 10000 = 0 then printfn "States: %i Queue: %i" (Set.count states) (List.length queue)
         let enqueue next (progress, states, queue) =
             if Set.contains next states then progress, states, queue
             else (progress + 1), (Set.add next states), next :: queue
-        let uu = applyTransform current u
-        let rr = applyTransform current r
-        let ff = applyTransform current f
-        (progress, states, queue) |> enqueue uu |> enqueue rr |> enqueue ff
+        let u = applyTransform current twistU
+        let r = applyTransform current twistR
+        let f = applyTransform current twistF
+        (progress, states, queue) |> enqueue u |> enqueue r |> enqueue f // avoiding DLB corner
     let rec go progress states = function
         | current :: queue ->
             let progress', states', queue' = nextPly progress states queue current
@@ -52,20 +24,30 @@ let gen () =
         | [] -> states
     go 0 Set.empty [solvedState]
 
-// this takes about a half-hour and 2GB RAM on a MacBook (generates 3,674,160 unique states)
-gen () |> Set.count |> printfn "Done: %i"
+let getOrderedStates () =
+    // ordered by hash code (allows for quicker reconstitution to simple array with binary search retrieval)
+    if statesPersisted () then
+        printf "%A Loading states (~15 sec)..." (DateTime.Now)
+        let states = loadStates numStates
+        states.Length |> printfn "\n%A Total States: %i" (DateTime.Now)
+        states
+    else
+        let rawStates = getRawStates ()
+        rawStates |> Set.count |> printfn "%A Total States: %i" (DateTime.Now)
+        printfn "%A Sorting (~3 min)..." (DateTime.Now)
+        let orderedStates = rawStates |> Set.toList |> Array.ofList |> Array.sort
+        printfn "%A Saving states (~17.5MB)..." (DateTime.Now)
+        saveStates orderedStates
+        orderedStates
 
-// let apply transform cube = function
+let states = getOrderedStates ()
 
-// let identity =
+printfn "Testing..."
+let foo = [|1; 2; 3; 0; 4; 5; 6; 7|], [|0; 0; 0; 0; 0; 0; 0; 0|]
+let bar = Twist L |> moveToTransform |> applyTransform solvedState
+let baz = Rotation X |> moveToTransform |> applyTransform solvedState
+let i = baz |> canonicalize |> findStateIndex states
+printfn "Found (%i): %A" i states.[i]
 
-// let invert t =
-
-// let combine t0 t1 =
-
-// let multiply t n =
-// < 0 -> invert t -n
-// = 0 -> identity
-// = 1 -> t
-//   2 -> combine t t
-// > 2 -> recurse
+printfn "%A Done! (press return to exit)" (DateTime.Now)
+Console.ReadLine() |> ignore
---
+++ solver222/Utility.fs
@@ -0,0 +1,187 @@
+﻿module Utility
+
+open System
+open System.IO
+open System.Collections.Generic
+
+type State = int array * int array
+
+let solvedState : State = [|0; 1; 2; 3; 4; 5; 6; 7|], [|0; 0; 0; 0; 0; 0; 0; 0|]
+
+type Transform = int array * int array
+
+let identityTransform = solvedState |> Transform
+
+type Twist = U | D | L | R | F | B
+
+let twistToTransform = function
+    | U -> [|1; 2; 3; 0; 4; 5; 6; 7|], [|0; 0; 0; 0; 0; 0; 0; 0|] : Transform
+    | D -> [|0; 1; 2; 3; 5; 6; 7; 4|], [|0; 0; 0; 0; 0; 0; 0; 0|] : Transform
+    | L -> [|0; 1; 6; 2; 4; 3; 5; 7|], [|0; 0; 2; 1; 0; 2; 1; 0|] : Transform
+    | R -> [|4; 0; 2; 3; 7; 5; 6; 1|], [|2; 1; 0; 0; 1; 0; 0; 2|] : Transform
+    | F -> [|3; 1; 2; 5; 0; 4; 6; 7|], [|1; 0; 0; 2; 2; 1; 0; 0|] : Transform
+    | B -> [|0; 7; 1; 3; 4; 5; 2; 6|], [|0; 2; 1; 0; 0; 0; 2; 1|] : Transform
+
+let twistU = U |> twistToTransform
+let twistD = D |> twistToTransform
+let twistL = L |> twistToTransform
+let twistR = R |> twistToTransform
+let twistF = F |> twistToTransform
+let twistB = B |> twistToTransform
+
+type Rotation = X | Y | Z
+
+let rotationToTransform = function
+    | Z -> [|3; 2; 6; 5; 0; 4; 7; 1|], [|1; 2; 1; 2; 2; 1; 2; 1|] : Transform
+    | X -> [|4; 0; 3; 5; 7; 6; 2; 1|], [|2; 1; 2; 1; 1; 2; 1; 2|] : Transform
+    | Y -> [|1; 2; 3; 0; 7; 4; 5; 6|], [|0; 0; 0; 0; 0; 0; 0; 0|] : Transform
+
+let rotateX = X |> rotationToTransform
+let rotateY = Y |> rotationToTransform
+let rotateZ = Z |> rotationToTransform
+
+type Move = Twist of Twist | Rotation of Rotation
+
+let moveToTransform = function
+    | Twist t -> twistToTransform t
+    | Rotation r -> rotationToTransform r
+
+// let invert t =
+
+// let combine t0 t1 =
+
+// let multiply t n =
+// < 0 -> invert t -n
+// = 0 -> identity
+// = 1 -> t
+//   2 -> combine t t
+// > 2 -> recurse
+
+let applyTransform ((ps, os) : State) ((pt, ot) : Transform) : State =
+    Array.init 8 (fun i -> ps.[pt.[i]]),
+    Array.init 8 (fun i -> (os.[pt.[i]] + ot.[i]) % 3)
+
+let combine (t0 : Transform) t1 = applyTransform (State t0) t1 |> Transform
+
+let twistU2  = combine twistU  twistU
+let twistD2  = combine twistD  twistD
+let twistL2  = combine twistL  twistL
+let twistR2  = combine twistR  twistR
+let twistF2  = combine twistF  twistF
+let twistB2  = combine twistB  twistB
+let rotateX2 = combine rotateX rotateX
+let rotateY2 = combine rotateY rotateY
+let rotateZ2 = combine rotateZ rotateZ
+
+let invert ((p, o) : Transform) : Transform =
+    let p' = Array.zeroCreate 8
+    let o' = Array.zeroCreate 8
+    for i in 0 .. 7 do
+        let from = p.[i]
+        p'.[from] <- i
+        o'.[from] <- (3 - o.[i] + 3) % 3
+    (p', o') |> Transform
+
+let twistU'  = invert twistU
+let twistD'  = invert twistD
+let twistL'  = invert twistL
+let twistR'  = invert twistR
+let twistF'  = invert twistF
+let twistB'  = invert twistB
+let rotateX' = invert rotateX
+let rotateY' = invert rotateY
+let rotateZ' = invert rotateZ
+
+let rotateXY   = combine rotateX  rotateY
+let rotateXY'  = combine rotateX  rotateY'
+let rotateXY2  = combine rotateX  rotateY2
+let rotateXZ   = combine rotateX  rotateZ
+let rotateXZ'  = combine rotateX  rotateZ'
+let rotateXZ2  = combine rotateX  rotateZ2
+let rotateYZ'  = combine rotateY  rotateZ'
+let rotateZX'  = combine rotateZ  rotateX'
+let rotateZY   = combine rotateZ  rotateY
+let rotateX'Z' = combine rotateX' rotateZ'
+let rotateX2Y  = combine rotateX2 rotateY
+let rotateX2Y' = combine rotateX2 rotateY'
+let rotateX2Z  = combine rotateX2 rotateZ
+let rotateX2Z' = combine rotateX2 rotateZ'
+
+let distinctRotations state = seq {
+    yield state
+    yield state |> applyTransform rotateX
+    yield state |> applyTransform rotateXY
+    yield state |> applyTransform rotateXY'
+    yield state |> applyTransform rotateXY2
+    yield state |> applyTransform rotateXZ
+    yield state |> applyTransform rotateXZ'
+    yield state |> applyTransform rotateXZ2
+    yield state |> applyTransform rotateX'
+    yield state |> applyTransform rotateYZ'
+    yield state |> applyTransform rotateZX'
+    yield state |> applyTransform rotateZY
+    yield state |> applyTransform rotateX'Z'
+    yield state |> applyTransform rotateX2
+    yield state |> applyTransform rotateX2Y
+    yield state |> applyTransform rotateX2Y'
+    yield state |> applyTransform rotateX2Z
+    yield state |> applyTransform rotateX2Z'
+    yield state |> applyTransform rotateY
+    yield state |> applyTransform rotateY'
+    yield state |> applyTransform rotateY2
+    yield state |> applyTransform rotateZ
+    yield state |> applyTransform rotateZ'
+    yield state |> applyTransform rotateZ2 }
+
+let isCanonical ((p, o) : State) = // DLB corner fixed
+    p.[6] = 6 && o.[6] = 0
+
+let canonicalize = distinctRotations >> Seq.filter isCanonical >> Seq.head
+
+let file = "../../../States222.bin"
+
+let saveStates states =
+    let writeState (bw : BinaryWriter) ((p, o) : State) =
+        let compress bits s =
+            let rec compress' v = function
+                | i :: tail -> compress' ((v <<< bits) ||| i) tail
+                | [] -> if bits = 3 then bw.Write(byte (v >>> 16))
+                        bw.Write(byte (v >>> 8))
+                        bw.Write(byte v)
+            s |> List.ofArray |> compress' 0
+        compress 3 p
+        compress 2 o
+    use bw = new BinaryWriter(new FileStream(file, FileMode.Create))
+    Array.iter (writeState bw) states
+
+let statesPersisted () = File.Exists file
+
+let loadStates numStates =
+    let readState (br : BinaryReader) : State =
+        let readVal bits mask =
+            let decompress v =
+                let rec decompress' v = seq {
+                    yield int (v &&& mask)
+                    yield! decompress' (v >>> bits) }
+                decompress' v |> Seq.take 8 |> Seq.rev |> Array.ofSeq
+            let b0 = if bits = 3 then br.ReadByte() |> int else 0
+            let b1 = br.ReadByte() |> int
+            let b2 = br.ReadByte() |> int
+            (b0 <<< 16) ||| (b1 <<< 8) ||| b2 |> decompress
+        readVal 3 0b111, readVal 2 0b11
+    use br = new BinaryReader(File.Open(file, FileMode.Open))
+    let read i =
+        if i % 100000 = 0 then printf "."
+        readState br
+    Array.init numStates read
+
+type StateComparer() =
+    interface IComparer<State> with
+        member this.Compare(a, b) =
+            if a < b then -1
+            elif a > b then 1
+            else 0
+let stateComparer = StateComparer()
+
+let findStateIndex states s =
+    Array.BinarySearch(states, s, stateComparer)
